2ec80d97792f203c6fadb4471e2a2837bc589226
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-10-26 00:49:27 +0100

    feat: Finish architecture

--- webgl/threejs/proyecto.js
+++ webgl/threejs/proyecto.js
@@ -17,61 +17,73 @@ let path = "images/";
 let ladosCara = altos = [];
 let l = b = -4;
 let r = t = -l;
-let room, cabeza, cuello, menton, boca, cara, nasal, frontal, paretal;
+let suelo, room, cabeza, cuello, menton, boca, cara, nasal, frontal, paretal;
 // Materiales y Texturas
-let materiales = [], textures = [];
-let controles = [];
-let inicia = -227, resta = 54;
-controles[0] = [inicia, false];
-controles[1] = [inicia -= resta, false];
-controles[2] = [inicia -= resta, false];
-controles[3] = [inicia -= resta, false];
-controles[4] = [inicia -= resta, false];
-controles[5] = [inicia -= resta, false];
-controles[6] = [inicia -= resta, false];
-
-console.log(controles);
+let materialSuelo = [], materiales = [], textures = [];
+let objetos = [], acciones = [];
 
 // Acciones
 init();
 loadScene();
 render();
 
+let inicia = -227, resta = 66;
+objetos[0] = [scene, suelo, inicia, false];
+objetos[1] = [cabeza, cuello, inicia -= resta, false];
+resta = 54;
+objetos[2] = [cabeza, menton, inicia -= resta, false];
+objetos[3] = [cabeza, boca, inicia -= resta, false];
+objetos[4] = [cabeza, cara, inicia -= resta, false];
+objetos[5] = [cabeza, nasal, inicia -= resta, false];
+objetos[6] = [cabeza, frontal, inicia -= resta, false];
+objetos[7] = [cabeza, paretal, inicia -= resta, false];
+for (let i = 0; i < objetos.length; i++) {
+    acciones[0] = [i, inicia -= resta, false];
+}
+
+console.log(objetos);
+console.log(acciones);
 let rellax = new Rellax('.rellax', {
     // center: true
     callback: function (position) {
         // callback every position change
         console.log(position);
-        if (position.y <= controles[0][0] && !controles[0][1]) {
-            cabeza.add(cuello);
-            controles[0][1] = true;
+        for (let i = 0; i < objetos.length; i++) {
+            if (position.y <= objetos[i][2] && !objetos[i][3]) {
+                console.log(position);
+                objetos[i][0].add(objetos[i][1]);
+                objetos[i][3] = true;
+            }
         }
-        if (position.y <= controles[1][0] && !controles[1][1]) {
-            cabeza.add(menton);
-            controles[1][1] = true;
+        for (let i = 0; i < acciones.length; i++) {
+            if (position.y <= acciones[i][1] && !acciones[i][2]) {
+                switch (i) {
+                    case 0:
+                        console.log(position);
+                        rebuildMaterials(acciones[i][0]);
+                        acciones[i][2] = true;
+                        break;
+                    case 1:
+                        console.log(position);
+                        updateObjectsMaterials(acciones[i][0]);
+                        acciones[i][2] = true;
+                        break;
+                    case 2:
+                        console.log(position);
+                        addShadows(acciones[i][0]);
+                        acciones[i][2] = true;
+                        break;
+                    case 3:
+                        generateLights(acciones[i][0]);
+                        console.log(position);
+                        acciones[i][2] = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
-        if (position.y <= controles[2][0] && !controles[2][1]) {
-            cabeza.add(boca);
-            controles[2][1] = true;
-        }
-        if (position.y <= controles[3][0] && !controles[3][1]) {
-            cabeza.add(cara);
-            controles[3][1] = true;
-        }
-        if (position.y <= controles[4][0] && !controles[4][1]) {
-            cabeza.add(nasal);
-            controles[4][1] = true;
-        }
-        if (position.y <= controles[5][0] && !controles[5][1]) {
-            cabeza.add(frontal);
-            controles[5][1] = true;
-        }
-        if (position.y <= controles[6][0] && !controles[6][1]) {
-            cabeza.add(paretal);
-            rebuildMaterials();
-            updateObjectsMaterials();
-            controles[6][1] = true;
-        }
+
     },
     breakpoints: [576, 768, 1024]
 });
@@ -80,6 +92,7 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(ancho, alto);
     renderer.setClearColor(new THREE.Color(0x363635));
+    renderer.shadowMap.enabled = true;
     document.getElementById('avance').appendChild(renderer.domElement);
 
     scene = new THREE.Scene();
@@ -87,9 +100,10 @@ function init() {
     let ar = ancho / alto;
     setCameras(ar);
 
-    // Controlador de camara
     cameraController = new THREE.OrbitControls( camera, renderer.domElement );
     cameraController.target.set(0,0,0);
+
+    window.addEventListener('resize', updateAspectRatio(ar));
 }
 
 function loadScene() {
@@ -98,6 +112,7 @@ function loadScene() {
     generateTextures();
     generateMaterials();
     // Geometrias
+    const geosuelo = new THREE.PlaneGeometry(500, 500, 50, 50);
     const geocuello = new THREE.CylinderGeometry(10, 10, altosCara[0], ladosCara[0]);
     const geomenton = new THREE.CylinderGeometry(25, 23, altosCara[1], ladosCara[1]);
     const geoboca = new THREE.CylinderGeometry(25, 25, altosCara[2], ladosCara[1]);
@@ -106,9 +121,13 @@ function loadScene() {
     const geofrontal = new THREE.CylinderGeometry(20, 24, altosCara[5], ladosCara[2]);
     const geoparetal = new THREE.CylinderGeometry(5, 20, altosCara[6], ladosCara[2]);
 
-    let indice = 0;
+    suelo = new THREE.Mesh(geosuelo, materialSuelo[0]);
+    suelo.position.set(0,0,0);
+    suelo.rotation.x += -1 * Math.PI/2;
+
     cabeza = new THREE.Object3D();
 
+    let indice = 0;
     let sube = altosCara[indice]/2;
     cuello = new THREE.Mesh(geocuello, materiales[indice]);
     cuello.position.set(0,sube,0);
@@ -193,6 +212,35 @@ function setCameras(ar){
     scene.add(camera);
 }
 
+function generateLights() {
+    let luzAmbiente = new THREE.AmbientLight(0xFFFFFF, 0.5);
+    scene.add(luzAmbiente);
+
+    let luzPuntual = new THREE.PointLight(0xFFFFFF, 0.7);
+    luzPuntual.position.set(100, 200, 100);
+    scene.add(luzPuntual);
+
+    let luzDireccional = new THREE.DirectionalLight(0xFFFFFF, 0.3);
+    luzDireccional.position.set(0, 40, -100);
+    scene.add(luzDireccional);
+
+    let luzFocal = new THREE.SpotLight(0xFFFFFF, 1);
+    luzFocal.position.set(0, 173, -100);
+    luzFocal.target.position.set(0, 0, 0);
+    luzFocal.angle = Math.PI / 10;
+    luzFocal.penumbra = 0.2;
+
+    luzFocal.shadow.camera.near = 100;
+    luzFocal.shadow.camera.far = 1700;
+    luzFocal.shadow.camera.fov = 7000;
+    luzFocal.shadow.mapSize.width = 500;
+    luzFocal.shadow.mapSize.height = 500;
+
+    scene.add(luzFocal.target);
+    luzFocal.castShadow = true;
+    scene.add(luzFocal);
+}
+
 function addRoom() {
     let shader = THREE.ShaderLib.cube;
     shader.uniforms.tCube.value = textures[2];
@@ -205,21 +253,21 @@ function addRoom() {
         side: THREE.BackSide
     });
 
-    room = new THREE.Mesh(new THREE.BoxGeometry(1000, 1000, 1000), shaderMaterial);
-    room.position.y += 500;
+    room = new THREE.Mesh(new THREE.BoxGeometry(500, 500, 500), shaderMaterial);
+    room.position.y += 250;
     scene.add(room);
 }
 
 function generateTextures() {
     // Materiales
-    let textureLoader = new THREE.TextureLoader()
-    textures[0] = textureLoader.load(path + 'wood.jpg');
+    let textureLoader = new THREE.TextureLoader();
+    textures[0] = textureLoader.load(path + 'pisometal.jpg');
     textures[0].magFilter = THREE.LinearFilter;
     textures[0].minFilter = THREE.LinearFilter;
     textures[0].repeat.set(4, 3);
     textures[0].wrapS = textures[0].wrapT = THREE.MirroredRepeatWrapping;
-    textures[1] = textureLoader.load(path + 'oxidado.jpg');
-    textures[2] = textureLoader.load(path + 'gold.jpg');
+    textures[1] = textureLoader.load(path + 'face.jpg');
+    textures[2] = textureLoader.load(path + 'newgold.jpg');
     let walls = [path + 'Yokohama/posx.jpg', path + 'Yokohama/negx.jpg',
         path + 'Yokohama/posy.jpg', path + 'Yokohama/negy.jpg',
         path + 'Yokohama/posz.jpg', path + 'Yokohama/negz.jpg'
@@ -229,97 +277,90 @@ function generateTextures() {
 
 function generateMaterials() {
     // Materiales
-    materiales[0] = new THREE.MeshBasicMaterial({
+    materialSuelo[0] = new THREE.MeshBasicMaterial({
         color: new THREE.Color("rgb(193, 51, 255)"),
         wireframe:true
     });
-    materiales[1] = new THREE.MeshBasicMaterial({
+    materiales[0] = new THREE.MeshBasicMaterial({
         color: new THREE.Color("rgb(255, 64, 0)"),
         wireframe:true
     });
-    materiales[2] = new THREE.MeshBasicMaterial({
+    materiales[1] = new THREE.MeshBasicMaterial({
         color: new THREE.Color("rgb(255, 191, 0)"),
         wireframe:true
     });
-    materiales[3] = new THREE.MeshBasicMaterial({
+    materiales[2] = new THREE.MeshBasicMaterial({
         color: new THREE.Color("rgb(64, 255, 0)"),
         wireframe:true
     });
-    materiales[4] = new THREE.MeshBasicMaterial({
+    materiales[3] = new THREE.MeshBasicMaterial({
         color: new THREE.Color("rgb(0, 128, 255)"),
         wireframe:true
     });
-    materiales[5] = new THREE.MeshBasicMaterial({
+    materiales[4] = new THREE.MeshBasicMaterial({
         color: new THREE.Color("rgb(255, 0, 191)"),
         wireframe:true
     });
-    materiales[6] = new THREE.MeshBasicMaterial({
+    materiales[5] = new THREE.MeshBasicMaterial({
         color: new THREE.Color("rgb(0, 255, 255)"),
         wireframe:true
     });
-    materiales[7] = new THREE.MeshBasicMaterial({
+    materiales[6] = new THREE.MeshBasicMaterial({
         color: new THREE.Color("rgb(255, 255, 0)"),
-        wireframe:true
-    });
-    materiales[8] = new THREE.MeshBasicMaterial({
-        color: new THREE.Color("rgb(255, 255, 255)"),
         wireframe:true
     });
 }
 
 function rebuildMaterials() {
     // Materiales
+    materialSuelo[0] = new THREE.MeshLambertMaterial({
+        color: "white",
+        map: textures[0]
+    });
     materiales[0] = new THREE.MeshLambertMaterial({
-        color: new THREE.Color("rgb(220, 172, 74)"),
+        color: new THREE.Color("rgb(169, 158, 154)"),
         map: textures[1]
     });
     materiales[1] = new THREE.MeshPhongMaterial({
-        color: new THREE.Color("rgb(122, 49, 19)"),
-        specular: new THREE.Color("rgb(214, 175,58)"),
+        color: new THREE.Color("rgb(169, 158, 154)"),
+        specular: new THREE.Color("rgb(143, 134, 135)"),
         shininess: 50,
         wireframe: false,
         map: textures[1]
     });
     materiales[2] = new THREE.MeshLambertMaterial({
-        color: new THREE.Color("rgb(220, 172, 74)"),
+        color: new THREE.Color("rgb(184, 150, 61)"),
         map: textures[2]
     });
     materiales[3] = new THREE.MeshPhongMaterial({
-        color: new THREE.Color("rgb(122, 49, 19)"),
-        specular: new THREE.Color("rgb(214, 175,58)"),
+        color: new THREE.Color("rgb(169, 158, 154)"),
+        specular: new THREE.Color("rgb(143, 134, 135)"),
         shininess: 50,
         wireframe: false,
         map: textures[1]
     });
     materiales[4] = new THREE.MeshLambertMaterial({
-        color: new THREE.Color("rgb(220, 172, 74)"),
+        color: new THREE.Color("rgb(184, 150, 61)"),
         map: textures[2]
     });
     materiales[5] = new THREE.MeshPhongMaterial({
-        color: new THREE.Color("rgb(122, 49, 19)"),
-        specular: new THREE.Color("rgb(214, 175,58)"),
+        color: new THREE.Color("rgb(169, 158, 154)"),
+        specular: new THREE.Color("rgb(143, 134, 135)"),
         shininess: 50,
         wireframe: false,
         map: textures[1]
     });
     materiales[6] = new THREE.MeshPhongMaterial({
-        color: new THREE.Color("rgb(122, 49, 19)"),
-        specular: new THREE.Color("rgb(214, 175,58)"),
+        color: new THREE.Color("rgb(169, 158, 154)"),
+        specular: new THREE.Color("rgb(143, 134, 135)"),
         shininess: 50,
         wireframe: false,
         map: textures[1]
     });
-    materiales[7] = new THREE.MeshBasicMaterial({
-        color: new THREE.Color("rgb(255, 255, 0)"),
-        wireframe:true
-    });
-    materiales[8] = new THREE.MeshBasicMaterial({
-        color: new THREE.Color("rgb(255, 255, 255)"),
-        wireframe:true
-    });
 }
 
 function updateObjectsMaterials() {
+    suelo.material = materialSuelo[0];
     let indice = 0;
     cuello.material = materiales[indice++];
     menton.material = materiales[indice++];
@@ -330,7 +371,46 @@ function updateObjectsMaterials() {
     paretal.material = materiales[indice++];
 }
 
+function addShadows() {
+    suelo.receiveShadow = true;
+    cuello.receiveShadow = true;
+    cuello.castShadow = true;
+    menton.receiveShadow = true;
+    menton.castShadow = true;
+    boca.receiveShadow = true;
+    boca.castShadow = true;
+    cara.receiveShadow = true;
+    cara.castShadow = true;
+    nasal.receiveShadow = true;
+    nasal.castShadow = true;
+    frontal.receiveShadow = true;
+    frontal.castShadow = true;
+    paretal.receiveShadow = true;
+    paretal.castShadow = true;
+}
+
 function buildHead() {
     altosCara = [10, 3, 5, 14, 3, 3, 4];
     ladosCara = [50, 20, 30];
+}
+
+function updateAspectRatio(ar)
+{
+    renderer.setSize(ancho, alto);
+
+    if(ar > 1){
+        alzado.left = perfil.left = planta.left = l * ar;
+        alzado.right = perfil.right = planta.right = r * ar;
+        alzado.top = perfil.top = planta.top = t;
+        alzado.bottom = perfil.bottom = planta.bottom = b;
+    }
+    else{
+        alzado.left = perfil.left = planta.left = l;
+        alzado.right = perfil.right = planta.right = r;
+        alzado.top = perfil.top = planta.top = t/ar;
+        alzado.bottom = perfil.bottom = planta.bottom = b/ar;
+    }
+
+    camera.aspect = ar;
+    camera.updateProjectionMatrix();
 }
